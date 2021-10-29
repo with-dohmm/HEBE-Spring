d3416735a2f4366709a4de348ff0ef251b755af0
SUE <dbb025284@naver.com>
2021-10-29 16:56:42 +0900

    ses add

---
+++ src/main/java/com/hebe/jwt/SES/EmailSenderDto.java
@@ -0,0 +1,44 @@
+package com.hebe.jwt.SES;
+
+import com.amazonaws.services.simpleemail.model.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class EmailSenderDto {
+
+    public static final String FROM_EMAIL = "hebenote.com@gmail.clom"; // 보내는 사람
+
+    private final List<String> to; // 받는 사람
+    private final String subject; // 제목
+    private final String content; // 본문
+
+    @Builder
+    public EmailSenderDto(final List<String> to, final String subject,
+                          final String content) {
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public SendEmailRequest toSendRequestDto() {
+        final Destination destination = new Destination()
+                .withToAddresses(this.to);
+
+        final Message message = new Message()
+                .withSubject(createContent(this.subject))
+                .withBody(new Body()
+                        .withHtml(createContent(this.content)));
+
+        return new SendEmailRequest()
+                .withSource(FROM_EMAIL)
+                .withDestination(destination)
+                .withMessage(message);
+    }
+
+    private Content createContent(final String text) {
+        return new Content()
+                .withCharset("UTF-8")
+                .withData(text);
+    }
+}
